6ffa6ac8c32dec67ecbdf016c02c7ad6e7484d54
PobCh <590650@gmail.com>
2018-02-22 22:00:09 +0700

    remove 'not understand' msg coz it conflicts with text generated from pushing a button

--- app.py
+++ app.py
@@ -169,9 +169,9 @@ def handle_text_message(event):
 
     if text in case:
         case[text]()
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            [TextSendMessage(text = 'See ya')]
-        )
+    # else:
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         [TextSendMessage(text = 'See ya')]
+    #     )
     
